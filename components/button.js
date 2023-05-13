d4fb3faf4f7b7b04fda2c9598ce29379ded7a785
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-05-13 17:02:47 +0300

    tweak button

--- components/button.js
+++ components/button.js
@@ -2,16 +2,4 @@
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-export const Button = ({}) => (
-  <div
-    style={{
-      width: "100vw",
-      height: "100vh",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    }}
-  >
-    <ConnectButton />
-  </div>
-);
+export const Button = ({}) => <ConnectButton />;
